04cdbf8aea7816c036cb058a8596c81eeb075d83
Noah Kim <noahbkim@gmail.com>
2017-04-21 20:31:43 -0400

    Export mode for loading into DjangoCTF.

--- __init__.py
+++ __init__.py
@@ -1,27 +1,24 @@
 import os
+import re
 import yaml
+import shutil
 import markdown
 import typing
 
 
-REQUIRED = {
-    "name": str,
-    "value": int,
-    "text": markdown.markdown,
-    "hint": markdown.markdown,
-    "flag": str}
-OPTIONAL = {
-    "author": str}
+REQUIRED = {"title": str, "value": int, "text": str, "hint": str, "flag": str}
+OPTIONAL = {"author": str}
 
 
 class Problem:
     """Default problem with an empty deployment method."""
 
-    def __init__(self, **config):
+    def __init__(self, category: str, name: str, **config):
         """Initialize the problem based on the configuration."""
 
-        self.name = config["name"]
-        self.category = config["category"]
+        self.category = category
+        self.name = name
+        self.title = config["title"]
         self.value = int(config["value"])
         self.text = config["text"]
         self.hint = config["hint"]
@@ -30,30 +27,59 @@ class Problem:
         self.files = config.get("files")
         self.enabled = config.get("enabled", True)
 
-    def export(self):
-        pass
+        self.replace = {}
+        if self.files:
+            for path in self.files:
+                basename = os.path.basename(path)
+                self.replace[os.path.basename(path)] = "/" + os.path.join(category, name, basename).replace(os.sep, "/")
 
-    def deploy(self, *args, **kwargs):
-        pass
+    def export(self, url=""):
+        """Export to a model corresponding to the Django API."""
+
+        text = self.text
+        for path, to in self.replace.items():
+            text = re.sub("\\{\\{\s*" + path + "\s*\\}\\}", url.rstrip("/") + to, text)
+        text = markdown.markdown(text)
+        hint = markdown.markdown(self.hint)
+        return {
+            "name": self.name,
+            "title": self.title,
+            "text": text,
+            "value": self.value,
+            "hint": hint,
+            "category": self.category,
+        }
+
+    def deploy(self, path: str):
+        """Deploy a normal, static problem. Moves files."""
+
+        to = os.path.join(path, self.name)
+        print("Copying files to {}".format(to))
+        for file in self.files:
+            shutil.copy(file, to)
 
 
 class DockerProblem(Problem):
     """Docker wrapper for isolated problems."""
 
-    def __init__(self, **config):
+    def __init__(self, category: str, name: str, **config):
         """Initialize a docker problem."""
 
-        super().__init__(**config)
+        super().__init__(category, name, **config)
         print("Created a docker problem.")
+
+    def deploy(self, path: str):
+        """Deploy the docker problem."""
+        pass
 
 
 class ShellProblem(Problem):
     """Shell problem model."""
 
-    def __init__(self, **config):
+    def __init__(self, category: str, name: str, **config):
         """Initialize a shell problem."""
 
-        super().__init__(**config)
+        super().__init__(category, name, **config)
         print("Created a shell problem.")
 
     def deploy(self, path: str):
@@ -68,19 +94,20 @@ PROBLEM = {
 }
 
 
-def load(category: str, path: str) -> Problem or None:
+def load(path: str) -> Problem or None:
     """Load a problem from its directory."""
 
     directory = os.path.dirname(path)
     reference = os.sep.join(directory.split(os.sep)[-2:])
-    config = {"category": category}
+    category, name = reference.split(os.sep)
+    config = {}
 
     with open(path) as file:
         raw = yaml.load(file)
 
     for field, cast in REQUIRED.items():
         if field not in raw or raw[field] is None:
-            print("Problem missing name in {}.".format(reference))
+            print("Problem missing title in {}.".format(reference))
             return None
         config[field] = cast(raw[field])
 
@@ -89,15 +116,17 @@ def load(category: str, path: str) -> Problem or None:
             config[field] = cast(raw[field])
 
     if "files" in raw:
-        files = raw["files"]
-        if not isinstance(files, list):
+        files = []
+        if not isinstance(raw["files"], list):
             print("Problem files for {} are not in list format.".format(reference))
             return None
-        for file in files:
+        for file in raw["files"]:
             file = str(file)
-            if not os.path.isfile(os.path.join(directory, file)):
+            full = os.path.join(directory, file)
+            if not os.path.isfile(full):
                 print("Problem file not found in {}: {}".format(reference, file))
                 return None
+            files.append(full)
         config["files"] = files
 
     if raw.get("enabled") is False:
@@ -116,7 +145,7 @@ def load(category: str, path: str) -> Problem or None:
                 print("No type specified in deploy in {}.".format(reference))
                 return None
 
-    return cast(**config)
+    return cast(category, name, **config)
 
 
 def search(directory: str) -> typing.List[Problem]:
@@ -150,7 +179,7 @@ def search(directory: str) -> typing.List[Problem]:
                 continue
             problem_path = os.path.join(problem_directory, "problem.yml")
             if os.path.isfile(problem_path):
-                problem_object = load(category, problem_path)
+                problem_object = load(problem_path)
                 if problem_object:
                     problems.append(problem_object)
 
--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import importlib
 import collections
+import json
 
 init = os.path.join(os.path.dirname(os.path.abspath(__file__)), "__init__.py")
 deploy = importlib.import_module("__init__", init)
@@ -11,6 +12,11 @@ subparsers = parser.add_subparsers(dest="command", metavar="command")
 
 search_parser = subparsers.add_parser("search", help="search for problems in a directory")
 search_parser.add_argument("path", help="the path to search")
+
+export_parser = subparsers.add_parser("export", help="export problems to a JSON file")
+export_parser.add_argument("path", help="the path to search")
+export_parser.add_argument("out", help="the JSON file path to write to")
+export_parser.add_argument("--url", help="URL to point static CTF files to", default=os.environ.get("CTF_URL", ""))
 
 
 namespace = parser.parse_args()
@@ -29,3 +35,19 @@ if namespace.command == "search":
     print("Total: {}".format(len(problems)))
     for category in frequency:
         print("  {}: {}".format(category.capitalize(), frequency[category]))
+
+
+if namespace.command == "export":
+
+    print("\nExporting...")
+    problems = deploy.search(namespace.path)
+    out = []
+    url = namespace.url
+    count = 0
+    for problem in problems:
+        if problem.enabled:
+            out.append(problem.export(url=namespace.url))
+            count += 1
+    with open(namespace.out, "w") as file:
+        json.dump(out, file, indent=4)
+    print("Exported {} problems to {}.".format(count, namespace.out))
